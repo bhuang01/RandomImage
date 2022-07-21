55f951f9b09bc73e5753d83d886768b3694821dd
Bryan Huang <b_huang@cs.ucsb.edu>
2022-07-20 23:05:28 -0700

    initial commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Bryan Huang on 7/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/RandomImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomImageApp.swift
+//  Shared
+//
+//  Created by Bryan Huang on 7/20/22.
+//
+
+import SwiftUI
+
+@main
+struct RandomImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
